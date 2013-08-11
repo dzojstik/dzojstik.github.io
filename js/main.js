284fe5142632b7fc198c2e2413c13a940f2959e1
dzojstik <adam.poland.89@gmail.com>
2013-08-11 10:39:29 +0200

    hehe

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
+//init masonry
 var container = document.querySelector('#container');
 var msnry = new Masonry(container, {
-	// options
 	columnWidth : 320,
 	itemSelector : '.item',
 	isFitWidth : true
 });
-
 $('#container').masonry({
 	itemSelector : '.item',
 	columnWidth : 320,
 	isFitWidth : true
 });
-//
-var extended = [];
-//array of extended tiles
-var tileExtendingNow = false;
-var previousItem;
-//if tile is extending now
+
+
+var extended = [];//array of extended tiles
+var tileExtendingNow = false;//if some tile is extending now
+var previousItem;//used to bring extending tile on top
+
+//click on tile event
 $('.item').click(function() {
 	if (tileExtendingNow != true)
 		$(this).stop();
@@ -31,6 +31,10 @@ $('.item').click(function() {
 				height : "173px",
 				width : "260px"
 			}, 90, 'linear', function() {
+				//show apropriate tile content
+				$(this).children('.smallTileContent').css('display', 'block');
+				$(this).children('.bigTileContent').css('display', 'none');
+				//and remove them from array extended
 				var index = extended.indexOf(this);
 				extended.splice(index, 1);
 				tileExtendingNow = false;
@@ -43,6 +47,11 @@ $('.item').click(function() {
 				height : "173px",
 				width : "260px"
 			}, 90, 'linear', function() {
+				//show apropriate tile content
+				$(this).children('.smallTileContent').css('display', 'block');
+				$(this).children('.bigTileContent').css('display', 'none');
+
+				//remove this from array extended
 				var index = extended.indexOf(this);
 				extended.splice(index, 1);
 				tileExtendingNow = false;
@@ -62,25 +71,31 @@ $('.item').click(function() {
 		previousItem.style.zIndex = 999;
 	this.style.zIndex = 1000;
 	previousItem=this;
+	
 	//start extending
 	$(this).animate({
 		height : "559px",
 		width : "580px",
 		opacity : 1
 	}, 100, 'linear', function() {
-		//restart masonry
+		//restart masonry layout
 		$('#container').masonry({
 			itemSelector : '.item',
 			columnWidth : 320,
 			isFitWidth : true
 		});
-
+		//show apropriate tile content
+		$(this).children('.smallTileContent').css('display', 'none');
+		$(this).children('.bigTileContent').css('display', 'block');
+		
 		tileExtendingNow = false;
 		extended.push(this);//add this tile to extended ones
 	})
 });
 
+//mouse over tile event
 $('.item').mouseover(function() {
+	//if is not in array extended
 	if ($.inArray(this, extended) <= -1) {
 		if (tileExtendingNow != true)
 			$(this).stop();
@@ -90,7 +105,9 @@ $('.item').mouseover(function() {
 	}
 
 });
+//mouse out of tile event
 $('.item').mouseout(function() {
+	//if is not in array extended
 	if ($.inArray(this, extended) <= -1) {
 		if (tileExtendingNow != true)
 			$(this).stop();
